0af52c29e5ad508185ca2fe693b6fa156675031f
Sungin Hong <hongsungin92@gmail.com>
2020-07-03 15:13:43 +0900

    #25.9 Creating a Message part One

---
+++ lists/templatetags/on_favs.py
@@ -0,0 +1,17 @@
+from django import template
+from lists import models as list_models
+
+register = template.Library()
+
+# context argument가 html에 입력받지않아도 입력된다
+@register.simple_tag(takes_context=True)
+def on_favs(context, room):
+    user = context.request.user
+    the_list = list_models.List.objects.get_or_none(
+        user=user, name="My Favourites Houses"
+    )
+    if the_list is not None:
+        return room in the_list.rooms.all()
+    else:
+        return the_list
+
